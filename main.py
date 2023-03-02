355694b51d9094fdce9adfe28519dfe333d90a24
JulianaYS <u20201b782@upc.edu.pe>
2023-03-02 02:15:31 -0500

    variable

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-
-#funcion para imprimir
+# funcion para imprimir
 print("Hola gente en internet")
 print('Hola gente sin internet')
+
+# VARIABLES
+variable = "Internet"
+print(variable)
+# no es necesario indicar el tipo
+variable = 2
+print(variable)
+# permite reutilizar variables
+x = 10
+y = 3
+z = x + y
+print(z)
+# cada variable apunta una direccion de memoria
+
+
